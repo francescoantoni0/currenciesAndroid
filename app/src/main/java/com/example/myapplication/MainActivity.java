eb432f8f6090a9a782901da1a657aa8e461352b7
Francesco Antoni <francesco.antoni@studenti.galileilivorno.edu.it>
2023-04-12 10:59:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button begin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        begin = findViewById(R.id.beginButton);
+        begin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.beginButton)
+            switchActivities();
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, ExchangeActivity.class);
+        startActivity(switchActivityIntent);
+    }
+}
