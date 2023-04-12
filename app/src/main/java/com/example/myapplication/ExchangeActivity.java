70b0aa0007f9c3ec3241cd96c6ae962b9b3900a8
Francesco Antoni <francesco.antoni@studenti.galileilivorno.edu.it>
2023-04-12 12:41:40 +0200

    Fixed graphics

--- app/src/main/java/com/example/myapplication/ExchangeActivity.java
+++ app/src/main/java/com/example/myapplication/ExchangeActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -16,6 +19,17 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityExchangeBinding;
 
-public class ExchangeActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class ExchangeActivity extends AppCompatActivity {
+    MaterialToolbar materialToolbar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange);
+        /*materialToolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(materialToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);*/
+    }
 }
